bbb39f764ef85a86f0ed11b86e775c52b88d9e31
DavidNovak <david.novak@email.cz>
2018-08-03 16:21:48 +0200

    Ciselniky-VERSION_COMPANY_pridani zaznamu po Action v Controlleru

--- VerzovaciSystem/VerzovaciSystem/Models/Entities/CompanyEntity.cs
+++ VerzovaciSystem/VerzovaciSystem/Models/Entities/CompanyEntity.cs
@@ -1,23 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using VerzovaciSystemDB;
 
 namespace VerzovaciSystem.Models.Entities
 {
     public class CompanyEntity
     {
+        DbRepository dbRepository = new DbRepository();
+
         // pro Vyhledávací masku
         public int Id { get; private set; }
-        public string Name { get; private set; }
+
+        [Required (ErrorMessage ="Vyplňte prosím")]
+        [MaxLength(100, ErrorMessage ="Příliš dlouhé. Zkraťte prosím na max. 99 znaků")]
+        public string Name { get; set; }
 
         // pro Číselníky
-        public string Active { get; private set; }
-        public string Description { get; private set; }
-        public string Interface { get; private set; }
-        public string Type { get; private set; }
-        public string Language { get; private set; }
+        [Required (ErrorMessage ="Vyberte prosím")]
+        public string Active { get;  set; }
 
+        [MaxLength(4000, ErrorMessage = "Příliš dlouhé. Zkraťte prosím na max. 3999 znaků")]
+        public string Description { get;  set; }
+
+        [MaxLength(4000, ErrorMessage = "Příliš dlouhé. Zkraťte prosím na max. 3999 znaků")]
+        public string Interface { get;  set; }
+
+        public string Type { get;  set; }
+
+        [MaxLength(50, ErrorMessage = "Příliš dlouhé. Zkraťte prosím na max. 49 znaků")]
+        public string Language { get;  set; }
+
+        // pro nový záznam do tabulky, DropDownList aktivní/neaktivní
+        public List<SelectListItem> Actives { get; private set; }
+
+        // pro nový záznam do tabulky, DropDownList  typ společnosti
+        public List<SelectListItem> Types { get; private set; }
 
         // pro Vyhledávací masku
         public CompanyEntity(int id, string name)
@@ -37,6 +58,22 @@ namespace VerzovaciSystem.Models.Entities
             Language = language;
         }
         public CompanyEntity()
-        { }
+        {
+            Actives = new List<SelectListItem>();
+            Actives.Add(new SelectListItem { Text = "Option", Value = null });
+            Actives.Add(new SelectListItem { Text = "Neaktivní", Value = "N" });
+            Actives.Add(new SelectListItem { Text = "Aktivní", Value = "A" });
+
+            List<EX_COMPANY_TYPE> companyTypesFromDB = dbRepository.GetCompanyTypes();
+
+            Types = new List<SelectListItem>();
+            Types.Add(new SelectListItem { Text = "option", Value = null });
+
+            foreach (var companyType in companyTypesFromDB)
+            {
+                Types.Add(new SelectListItem { Text = companyType.EX_DESC, Value = companyType.EX_COMPANY_TYPE1 });
+            }
+
+        }
     }
 }
---
+++ VerzovaciSystem/VerzovaciSystem/Models/TablesViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VerzovaciSystem.Models.Entities;
+using VerzovaciSystemDB;
+using VerzovaciSystem.Controllers;
+
+namespace VerzovaciSystem.Models
+{
+    public class TablesViewModel
+    {
+        DbRepository dbRepository = new DbRepository();
+
+        // popisky sloupců tabulek
+        public CompanyEntity CompanyEntity { get; set; }
+
+        // Seznam tabulek
+        public List<TableListEntity> TablesList { get; private set; }
+
+        public List<CompanyEntity> TableData { get; private set; }
+
+        // Seznam tabulek + objekty tabulek
+        public TablesViewModel()
+        {
+            TablesList = new List<TableListEntity>();
+            TablesList.Add(new TableListEntity("VERSION_COMPANY", "Seznam společností"));
+            TablesList.Add(new TableListEntity("VERSION_RELEASE_DATE", "Evidence vydání release verzí"));
+            TablesList.Add(new TableListEntity("VERSION_COMPANY_TNS_ALIAS", "Evidence připojení do společnosti"));
+            TablesList.Add(new TableListEntity("VERSION_COMPANY_BIGTABLE", "Evidence společností, které mají \"velké tabulky\" "));
+            TablesList.Add(new TableListEntity("VERSION_COMPANY_BIGTABLES", "Seznam \"velkých tabulek\" "));
+            TablesList.Add(new TableListEntity("EX_SCHEMA_EXCEPTIONS", "Seznam vyjímek pro konkrétní společnosti, které jsou vyloučené ze sychronizace schémat"));
+
+            CompanyEntity = new CompanyEntity();
+        }
+
+        // Data z vybrané tabulky
+        public void GetTableData (Tables tableName)
+        {
+            switch (tableName)
+            {
+                    case Tables.VERSION_COMPANY:
+            List<VERSION_COMPANY> companiesFromDB = dbRepository.GetCompanies();
+            TableData = companiesFromDB.Select(x => new CompanyEntity(HelpsMethods.GetIntValue(x.VER_COMPANY_ID), x.VER_COMPANY, x.VER_COMPANY_ACTIVE, x.VER_COMPANY_DESC, x.VER_COMPANY_INTERFACE, x.VER_COMPANY_TYPE, x.VER_COMPANY_LANGUAGE))
+                                       .OrderBy(a => a.Id)                        
+                                       .ToList();
+                    break;
+            }
+
+        }
+
+        
+
+    }
+}
